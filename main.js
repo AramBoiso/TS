2aab6070e2ff728b8c0b31a02c6aa6e6c6817094
Aram <boiso.ska8@gmail.com>
2018-01-02 14:17:20 -0600

    variables y tipos && diferencia entre var y let

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-function hola(n) {
-    return "Hola Mundo " + nombre;
+//variable y Tipos
+var na = "Aram", age = 19, programer = true, langs = ["php", "JS", "Go"], etiqueta = document.getElementById("container");
+etiqueta.innerHTML = na + " " + age + " " + programer + " " + langs[0];
+// var - let
+var a = 7;
+var b = 12;
+if (a === 7) {
+    var a_1 = 4;
+    b -= 1;
+    console.log(a_1 + " " + b);
 }
-var nombre = "Aram";
-var etiqueta = document.getElementById("container");
-etiqueta.innerHTML = hola(nombre);
--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
-function hola(n){
-    return "Hola Mundo "+nombre;
+//variable y Tipos
+
+var na:string = "Aram",
+   age:number = 19,
+   programer:boolean = true,
+   langs:Array<string> =["php","JS","Go"],
+   etiqueta = <HTMLElement>document.getElementById("container");
+
+    etiqueta.innerHTML = na+" "+age+" "+programer+" "+langs[0];
+
+
+    // var - let
+var a:number = 7;
+var b:number = 12;
+
+if(a === 7){
+	let a = 4;
+	 b -= 1;
+
+	console.log(a+" "+b);
 }
-
-var nombre = "Aram";
-
-var etiqueta = <HTMLElement>document.getElementById("container");
-etiqueta.innerHTML = hola(nombre);
